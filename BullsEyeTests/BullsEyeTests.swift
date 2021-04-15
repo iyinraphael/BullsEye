804feeed301ea9deab60f94ef991ad8298d8fe05
iyinraphael <iyinraphael@gmail.com>
2021-04-15 08:58:24 -0700

    asynchronous testing

--- BullsEyeTests/BullsEyeTests.swift
+++ BullsEyeTests/BullsEyeTests.swift
@@ -43,17 +43,16 @@ class BullsEyeTests: XCTestCase {
       try super.tearDownWithError()
 
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+  
+  func  testScoreIsComputedWhenGuessIsHigherThanTarget() {
+    //  given
+    let guess = sut.targetValue - 5
+    
+    //  when
+    sut.check(guess: guess)
+    
+    // then
+    XCTAssertEqual(sut.scoreRound, 95, "Score computed from guess is wrong")
+  }
 
 }
